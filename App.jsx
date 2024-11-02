7dbc2590aaa76a3d9306db4212e25ef96c7fddf4
SHAUKEEN KHAN <153415311+SHAUKEEN123@users.noreply.github.com>
2024-11-02 16:05:42 +0530

    Add files via upload
    npm install for node-modules

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+// import Products from './products';
+function App() {
+  // const a="shaukeen khan";
+  // var [a,b]=useState(69);
+  // const [a,b]= useState(false);
+  return (
+    <div className='w-full h-screen bg-zinc-900 text-white'>
+      {/* <h1>{a}</h1> */}
+      {/* <button onClick={()=>b(a+1)} className='px-3 py-1 bg-green-500 rounded-md text-xs'>ClickMe!</button> */}
+      {/* <h1 className={`${a===false? "text-red-600": "text-blue-600"}`}>{a===false?"hello":"hay ji"}</h1>
+       <button onClick={()=>b(!a)} className='px-3 py-1 bg-green-500 rounded-md text-xs'>ClickMe!</button> */}
+      {/* <Products /> */}
+      {/* {a} */}
+
+    </div>
+  )
+
+}
+
+export default App
---
+++ products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className='text-white'>
+      <h1>jab bhi component ko import karna hota h too function or export vali jgaha pr bhi name ka pahela world cappital hona chahiye </h1>
+    </div>
+  )
+}
+
+export default Products
